0b6cd36db3666facb89291e903f8d8f0a86f62d2
duongtran2003 <dt005250@gmail.com>
2026-06-18 23:15:34 +0700

    render pass manager with topology sort

--- src/core/render/render_pass_manager.h
+++ src/core/render/render_pass_manager.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include "core/render/render_pass.h"
+#include "vulkan/vulkan.hpp"
 #include <memory>
 #include <string>
+#include <type_traits>
 #include <unordered_map>
+#include <utility>
 #include <vector>
 
 namespace SimpleEngine {
@@ -16,10 +19,32 @@ private:
   bool isDirty = true;
 
 public:
+  // Need to implement it here
   template <typename T, typename... Args>
-  T *addRenderPass(const std::string &name, Args &&...args) {}
+  T *addRenderPass(const std::string &name, Args &&...args) {
+    static_assert(std::is_base_of<RenderPass, T>::value,
+                  "T must derive from RenderPass");
 
-  RenderPass* getRenderPass(const std::string& name);
+    auto renderPassIt = passes.find(name);
+    if (renderPassIt != passes.end()) {
+      return dynamic_cast<T *>(renderPassIt->second);
+    }
+
+    auto pass = std::make_unique<T>(std::forward<Args>(args)...);
+    T *passPtr = pass.get();
+    pass[name] = std::move(pass);
+    isDirty = true;
+
+    return passPtr;
+  }
+
+  RenderPass *getRenderPass(const std::string &name);
+  void removeRenderPass(const std::string &name);
+
+  void execute(vk::CommandBuffer &commandBuffer);
+
+private:
+  void sortPasses();
 };
 } // namespace Core
 } // namespace SimpleEngine
