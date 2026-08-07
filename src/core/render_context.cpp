6bfa332ee02bdfe62ad10680c842e5184e1816a5
Duong Tran <114930252+duongtran2003@users.noreply.github.com>
2026-08-07 23:21:55 +0700

    Change surface format from eB8G8R8A8Srgb to eB8G8R8A8Unorm

--- src/core/render_context.cpp
+++ src/core/render_context.cpp
@@ -370,7 +370,7 @@ void RenderContext::createSwapChain() {
   vk::SurfaceFormatKHR _swapChainSurfaceFormat;
   const auto formatIt = std::ranges::find_if(
       availableFormats, [](const vk::SurfaceFormatKHR &format) {
-        return format.format == vk::Format::eB8G8R8A8Srgb &&
+        return format.format == vk::Format::eB8G8R8A8Unorm &&
                format.colorSpace == vk::ColorSpaceKHR::eSrgbNonlinear;
       });
 
